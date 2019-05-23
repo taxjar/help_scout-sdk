e9632a27b7ae8688bb7c43abcc0ed854f9bdef95
Paul Simpson <prsimp@gmail.com>
2019-05-23 11:33:43 -0500

    Adds Conversation#update_tags

--- spec/unit/conversation_spec.rb
+++ spec/unit/conversation_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Helpscout::Conversation do
   include_examples 'getable unit', 'https://api.helpscout.net/v2/conversations/1'
   include_examples 'listable unit', "https://api.helpscout.net/v2/conversations?mailbox=#{Helpscout.default_mailbox}"
 
+  let(:conversation) { described_class.new(params) }
+
   describe '.create' do
     subject { described_class.create(params) }
     let(:params) { { subject: 'Hello World' } }
@@ -49,13 +51,38 @@ RSpec.describe Helpscout::Conversation do
       }
     end
 
-    subject { described_class.new(params).update(*update_params.values) }
+    subject { conversation.update(*update_params.values) }
 
     before do
-      stub_request(:patch, "https://api.helpscout.net/v2/conversations/#{id}").
-        with(body: update_params.to_json).
-        to_return(status: 204)
+      stub_request(:patch, "https://api.helpscout.net/v2/conversations/#{id}")
+        .with(body: update_params.to_json)
+        .to_return(status: 204)
     end
+
+    it "returns true" do
+      expect(subject).to be true
+    end
+  end
+
+  describe '#update_tags' do
+    let(:id) { "123" }
+    let(:params) do
+      {
+        id: id,
+        _links: {
+          self: { href: "https://api.helpscout.net/v2/conversations/#{id}" }
+        }
+      }
+    end
+    let(:tags) { %w[vip pro] }
+
+    before do
+      stub_request(:put, "https://api.helpscout.net/v2/conversations/#{id}/tags")
+        .with(body: { tags: tags }.to_json)
+        .to_return(status: 204)
+    end
+
+    subject { conversation.update_tags(tags) }
 
     it "returns true" do
       expect(subject).to be true
