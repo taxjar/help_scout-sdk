561fe88eca6e7990fda7c054f4bbc31873831094
Paul Simpson <prsimp@gmail.com>
2019-05-29 14:56:24 -0500

    WIP fixing specs [ci-skip]

--- spec/integration/access_token_spec.rb
+++ spec/integration/access_token_spec.rb
@@ -1,26 +1,11 @@
 # frozen_string_literal: true
 
-RSpec.describe Helpscout::AccessToken do
+RSpec.describe Helpscout::API::AccessToken do
   describe '.create' do
     subject { described_class.create }
 
-    it 'returns a Helpscout::AccessToken' do
-      expect(subject).to be_a Helpscout::AccessToken
-    end
-  end
-
-  describe '.update' do
-    let(:new_token_params) do
-      {
-        access_token: 'foo',
-        expires_in: 7200
-      }
-    end
-
-    subject { described_class.update(new_token_params) }
-
-    it 'returns a Helpscout::AccessToken' do
-      expect(subject).to be_a Helpscout::AccessToken
+    it 'returns an AccessToken' do
+      expect(subject).to be_a described_class
     end
   end
 end
--- spec/unit/access_token_spec.rb
+++ spec/unit/access_token_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Helpscout::AccessToken do
+RSpec.describe Helpscout::API::AccessToken do
   let(:access_token_json) { file_fixture('access_token.json') }
   let(:access_token_params) { JSON.parse(access_token_json, symbolize_names: true) }
   let(:access_token) { described_class.new(access_token_params) }
@@ -13,37 +13,10 @@ RSpec.describe Helpscout::AccessToken do
   describe '.create' do
     subject { described_class.create }
 
-    it { is_expected.to be_a Helpscout::AccessToken }
+    it { is_expected.to be_a described_class }
   end
 
-  describe '.update' do
-    let(:new_token_params) do
-      {
-        access_token: 'foo',
-        expires_in: 7200
-      }
-    end
-
-    subject { described_class.update(new_token_params) }
-
-    it 'sets the access_token configuration value' do
-      expect_any_instance_of(
-        Helpscout::Configuration
-      ).to receive(:access_token=)
-
-      subject
-    end
-
-    it 'sets the access_token on the API client' do
-      expect_any_instance_of(
-        Helpscout::API
-      ).to receive(:access_token=).with(new_token_params[:access_token])
-
-      subject
-    end
-  end
-
-  describe '#token' do
+  describe '#value' do
     subject { access_token.value }
 
     it { is_expected.to eq(access_token_params[:access_token]) }
