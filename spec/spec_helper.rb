495643bed5cb66aea304aff60f214b2c956da13b
Andre LeBlanc <andre@taxjar.com>
2019-05-31 01:06:48 -0400

    record none

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
-  config.default_cassette_options = { record: :once }
+  config.default_cassette_options = { record: :none }
   config.configure_rspec_metadata!
 
   config.filter_sensitive_data('<HELP_SCOUT_ACCESS_TOKEN>') { HelpScout.access_token.value }
